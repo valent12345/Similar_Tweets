eb4c1301a228102acaf0478d5641f952187fc707
SC <63264140+SC75@users.noreply.github.com>
2020-12-14 22:05:26 +0100

    Update stress_test_app.py

--- stress_test_app.py
+++ stress_test_app.py
@@ -4,9 +4,7 @@ import grequests
 import time
 import os
 
-
 class FlaskTests(unittest.TestCase):
-
     def setUp(self):
         os.environ['NO_PROXY'] = '0.0.0.0'
         pass
@@ -16,21 +14,19 @@ class FlaskTests(unittest.TestCase):
 
     def test_index(self):
         n = 1000
-        urls = []
+        resonses_urls = []
 
         for i in range(n):
-            urls.append('http://localhost:5000')
-
-        responses = [grequests.get(u) for u in urls]
+            responses_urls.append(requests.get('http://localhost:5000'))
 
         start = time.time()
-        simultaneous_responses = grequests.map(responses)
         end = time.time()
-        t = end - start
+        t = end-start
 
         for i in range(n):
-            self.assertEqual(simultaneous_responses[i].status_code, 200)
-        print('The {} requests took: {} seconds'.format(n, t))
+            self.assertEqual(responses_urls[i].status_code, 200)
+
+        print("The {} requests took: {} seconds".format(n, t))
 
 
 if __name__ == '__main__':
